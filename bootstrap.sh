c6ff66b05e71ec925ee0dac8cd07302a2bb5ca1a
Helmut Grohne <helmut@subdivi.de>
2014-04-27 12:09:46 +0200

    fix glibc @nobootstrap@ patch to not drop arbitrary deps
    The greediness of .* in the sed expression caused most dependencies to
    be silently discarded. Upon fixing this an error in the patched Depends:
    line surfaced and #745036 was reproduced with libc6-dev -> libc-dev-bin.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1673,7 +1673,7 @@ diff -Nru eglibc-2.18/debian/rules.d/control.mk eglibc-2.18/debian/rules.d/contr
  	cat debian/control.in/libnss-files-udeb	>> \$@T
 -	sed -e 's%@libc@%\$(libc)%g' < \$@T > debian/control
 +ifeq (\$(DEB_BUILD_PROFILE),bootstrap)
-+	sed -e 's%@libc@%\$(libc)%g;s%@nobootstrap@.*,%%g' < \$@T > debian/control
++	sed -e 's%@libc@%\$(libc)%g;s%@nobootstrap@[^,]*,%%g' < \$@T > debian/control
 +else
 +	sed -e 's%@libc@%\$(libc)%g;s%@nobootstrap@%%g' < \$@T > debian/control
 +endif
@@ -1761,7 +1761,7 @@ diff -Nru eglibc-2.18/debian/control.in/libc eglibc-2.18/debian/control.in/libc
  Priority: required
  Multi-Arch: same
 -Depends: \${shlibs:Depends}, libgcc1 [!hppa !m68k], libgcc2 [m68k], libgcc4 [hppa]
-+Depends: @nobootstrap@, libgcc1 [!hppa !m68k], @nobootstrap@ libgcc2 [m68k], @nobootstrap@ libgcc4 [hppa], \${shlibs:Depends}
++Depends: @nobootstrap@ libgcc1 [!hppa !m68k], @nobootstrap@ libgcc2 [m68k], @nobootstrap@ libgcc4 [hppa], \${shlibs:Depends}
  Recommends: libc6-i686 [i386], libc0.1-i686 [kfreebsd-i386], libc0.3-i686 [hurd-i386] 
  Suggests: glibc-doc, debconf | debconf-2.0, locales [!hurd-i386]
  Provides: \${locale-compat:Depends}, libc6-sparcv9b [sparc sparc64]
@@ -1770,10 +1770,10 @@ diff -Nru eglibc-2.18/debian/rules.d/control.mk eglibc-2.18/debian/rules.d/contr
 +++ eglibc-2.18/debian/rules.d/control.mk
 @@ -45,7 +45,11 @@
  ifeq (\$(DEB_BUILD_PROFILE),bootstrap)
- 	sed -e 's%@libc@%\$(libc)%g;s%@nobootstrap@.*,%%g' < \$@T > debian/control
+ 	sed -e 's%@libc@%\$(libc)%g;s%@nobootstrap@[^,]*,%%g' < \$@T > debian/control
  else
 +ifneq (\$(filter stage2,\$(DEB_BUILD_PROFILES)),)
-+	sed -e 's%@libc@%\$(libc)%g;s%@nobootstrap@.*,%%g' < \$@T > debian/control
++	sed -e 's%@libc@%\$(libc)%g;s%@nobootstrap@[^,]*,%%g' < \$@T > debian/control
 +else
  	sed -e 's%@libc@%\$(libc)%g;s%@nobootstrap@%%g' < \$@T > debian/control
 +endif
