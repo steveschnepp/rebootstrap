db29912602b9ae08f40b0bc76cd83065e53f5c84
Helmut Grohne <helmut@subdivi.de>
2014-12-06 14:27:18 +0100

    don't use non-existing shell function drop_privs yet

--- bootstrap.sh
+++ bootstrap.sh
@@ -493,7 +493,7 @@ EOF
 patch_gcc_4_9() {
 	if test "$ENABLE_MULTIARCH_GCC" = yes; then
 		echo "applying patches for with_deps_on_target_arch_pkgs"
-		drop_privs QUILT_PATCHES=/usr/share/cross-gcc/template/debian/patches/ quilt push -a
+		QUILT_PATCHES=/usr/share/cross-gcc/template/debian/patches/ quilt push -a
 	fi
 }
 if test "$ENABLE_MULTIARCH_GCC" = yes; then
