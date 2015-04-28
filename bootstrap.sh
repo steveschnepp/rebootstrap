e5d869053fcdbfd3e5096cc35ecec00ff2437e13
Helmut Grohne <helmut@subdivi.de>
2015-04-28 19:28:23 +0200

    drop hacks for using gcc-5 from experimental

--- bootstrap.sh
+++ bootstrap.sh
@@ -382,15 +382,7 @@ if test "$ENABLE_MULTIARCH_GCC" = yes; then
 fi
 
 obtain_source_package() {
-	if test "$1" = gcc-5; then
-		echo "deb-src $MIRROR experimental main" > /etc/apt/sources.list.d/tmp-experimental.list
-		apt-get update
-	fi
 	drop_privs apt-get source "$1"
-	if test -f /etc/apt/sources.list.d/tmp-experimental.list; then
-		rm /etc/apt/sources.list.d/tmp-experimental.list
-		apt-get update
-	fi
 }
 
 # work around dpkg bug #764216
@@ -953,23 +945,7 @@ EOF
 }
 # choosing libatomic1 arbitrarily here, cause it never bumped soname
 BUILD_GCC_MULTIARCH_VER=`apt-cache show --no-all-versions libatomic1 | sed 's/^Source: gcc-\([0-9.]*\)$/\1/;t;d'`
-if test "$GCC_VER" = 5 -a "$BUILD_GCC_MULTIARCH_VER" = "4.9"; then
-	echo "deb [ arch=`dpkg --print-architecture` ] $MIRROR experimental main" > /etc/apt/sources.list.d/tmp-experimental.list
-	apt-get update
-	if test "$ENABLE_MULTIARCH_GCC" = yes; then
-		cat >/etc/apt/preferences.d/experimental_unstable_gcc <<EOF
-Package: cpp* g++* gcc* libcilkrts* libasan* libatomic* libgcc* libgomp* libitm* liblsan* libquadmath* libstdc++* libtsan* libubsan*
-Pin: release a=experimental
-Pin-Priority: 1000
-EOF
-		$APT_GET dist-upgrade
-		rm /etc/apt/preferences.d/experimental_unstable_gcc
-	else
-		$APT_GET install "gcc-$GCC_VER-base" libcc1-0
-	fi
-	rm /etc/apt/sources.list.d/tmp-experimental.list
-	apt-get update
-elif test "$GCC_VER" != "$BUILD_GCC_MULTIARCH_VER"; then
+if test "$GCC_VER" != "$BUILD_GCC_MULTIARCH_VER"; then
 	echo "host gcc version ($GCC_VER) and build gcc version ($BUILD_GCC_MULTIARCH_VER) mismatch. need different build gcc"
 if test -d "$RESULT/gcc0"; then
 	echo "skipping rebuild of build gcc"
