b84b7a1dad3866efd4cfec532890bfeb5a7234ca
Helmut Grohne <helmut@subdivi.de>
2015-08-01 15:43:33 +0200

    avoid file conflict while upgrading gcc stages
    It seems that differently staged builds of gcc-5 put different contents
    into gcc-5-base. Now upgrading the stages of gcc-5-base can fail since,
    because we both install gcc-5-base for multiple architectures and
    upgrade it with the same version. Thus dpkg does not upgrade them in
    lockstep and fails unpacking either architecture since the previous
    (different) stage is still installed with the same version for the other
    architecture.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1669,6 +1669,8 @@ else
 	cd ..
 	ls -l
 	pickup_packages *.changes
+	# avoid file conflicts between differently staged M-A:same packages
+	apt_get_remove "gcc-$GCC_VER-base:$HOST_ARCH"
 	drop_privs rm -fv gcc-*-plugin-*.deb gcj-*.deb gdc-*.deb ./*objc*.deb ./*-dbg_*.deb
 	dpkg -i *.deb
 	compiler="`dpkg-architecture -a$HOST_ARCH -qDEB_HOST_GNU_TYPE`-gcc-$GCC_VER"
