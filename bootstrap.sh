a840d4da8bbbe1e552fc984012dff580c27b636e
Helmut Grohne <helmut@subdivi.de>
2016-02-25 21:07:24 +0100

    work around satisfiability issues involving experimental
    Since we pull gcc-6 from experimental and then remove experimental from
    sources.list, dose doesn't know that gcc-6 is available to the build
    architecture. It thus fails to find installation sets where libgcc1 or
    libstdc++6 need to be coinstalled.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4319,6 +4319,7 @@ Description: fake crossbuild-essential package for dose-builddebcheck
 
 EOF
 	sed -i -e '/^Conflicts:.* libc[0-9][^ ]*-dev\(,\|$\)/d' "$package_list" # also make dose ignore the glibc conflict
+	apt-cache show libgcc1=installed libstdc++6=installed >> "$package_list" # helps when pulling gcc from experimental
 	cat /var/lib/apt/lists/*_Sources > "$source_list"
 	errcode=0
 	dose-builddebcheck "--deb-native-arch=`dpkg --print-architecture`" "--deb-host-arch=$HOST_ARCH" "$@" "$package_list" "$source_list" || errcode=$?
