3b1b6c280e085549b6edf9ee34840383103062c1
Helmut Grohne <helmut@subdivi.de>
2016-11-23 19:22:06 +0100

    stop confusing the gcc packaging with a .pc directory

--- bootstrap.sh
+++ bootstrap.sh
@@ -1143,6 +1143,7 @@ patch_gcc_wdotap() {
 	if test "$ENABLE_MULTIARCH_GCC" = yes; then
 		echo "applying patches for with_deps_on_target_arch_pkgs"
 		drop_privs QUILT_PATCHES="/usr/share/cross-gcc/patches/gcc-$GCC_VER" quilt push -a
+		drop_privs rm -Rf .pc
 	fi
 }
 patch_gcc_5() {
