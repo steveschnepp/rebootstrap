f262812dfe7558af2480a0ddfb7c7505a6d364a5
Helmut Grohne <helmut@subdivi.de>
2017-11-09 05:39:12 +0100

    when pulling gcc from experimental, don't rely on gcc-defaults
    When new gcc versions are introduced into unstable, gcc-defaults in
    experimental does not immediately point at the new version. So for
    pulling the experimental gcc libraries we actually need to install that
    gcc version.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2377,7 +2377,7 @@ if test "$GCC_VER" != "$BUILD_GCC_MULTIARCH_VER"; then
 if dpkg --compare-versions "$GCC_VER" gt "$BUILD_GCC_MULTIARCH_VER"; then
 	echo "deb [ arch=$(dpkg --print-architecture) ] $MIRROR experimental main" > /etc/apt/sources.list.d/tmp-experimental.list
 	$APT_GET update
-	$APT_GET -t experimental install gcc g++
+	$APT_GET -t experimental install g++ g++-$GCC_VER
 	rm -f /etc/apt/sources.list.d/tmp-experimental.list
 	$APT_GET update
 elif test -f "$REPODIR/stamps/gcc_0"; then
