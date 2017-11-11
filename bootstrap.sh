dd191fb23607454b069dad1f3e96c4989a8d2a7c
Helmut Grohne <helmut@subdivi.de>
2017-11-11 15:53:38 +0100

    drop sh3 patch #851867 applied in glibc/2.24-12

--- bootstrap.sh
+++ bootstrap.sh
@@ -3000,8 +3000,6 @@ EOF
  		--without-selinux \
  		--enable-stackguard-randomization \
 EOF
-	echo "patching glibc for sh3 #851867"
-	drop_privs cp -nv debian/sysdeps/sh4.mk debian/sysdeps/sh3.mk
 }
 if test -f "$REPODIR/stamps/${LIBC_NAME}_1"; then
 	echo "skipping rebuild of $LIBC_NAME stage1"
